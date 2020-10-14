4e110c6aaec5ce2a369bd46501afae2478e0f6d8
NightmareXE <69788674+NightmareXE@users.noreply.github.com>
2020-10-14 22:11:45 +0500

    Add files via upload

---
+++ Verminium/Projectiles/LeadShower.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace Verminium.Projectiles
+{
+    public class LeadShower : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_" + ProjectileID.Bullet;
+
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 16;
+            projectile.friendly = true;
+            projectile.aiStyle = -1;
+            
+            projectile.rotation = MathHelper.ToRadians(90);
+        }
+        public override void AI()
+        {
+            Lighting.AddLight(projectile.Center, 0.02f, 0.01f, 0.01f);
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90);
+
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            //Slightly randomizing the spawn position
+            Vector2 spawnPos = new Vector2(Main.rand.NextBool(2) ? target.Center.X - 32 : target.Center.Y + 32, target.Center.Y - 640);
+            Vector2 vel = target.Center - spawnPos;
+            vel.Normalize();
+
+            
+            Projectile.NewProjectile(spawnPos, vel * 5, ModContent.ProjectileType<Tracker>(), damage, knockback, Main.myPlayer);
+
+
+        }
+    }
+}
---
+++ Verminium/Projectiles/Tracker.cs
@@ -0,0 +1,63 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace Verminium.Projectiles
+{
+    public class Tracker : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_" + ProjectileID.MeteorShot;
+        public override void SetStaticDefaults()
+        {
+            //THis is just classification
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = projectile.height = 16;
+            projectile.friendly = true;
+            projectile.rotation = MathHelper.ToRadians(90);
+            projectile.aiStyle = -1;
+        }
+        public override void AI()
+        {
+            Lighting.AddLight(projectile.Center, 0.02f, 0.01f, 0.01f);
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90);
+            Vector2 move = Vector2.Zero;
+            float Distance = 384f;
+            bool LockOn = false;
+            for (int i = 0; i < 200; i++)
+            {
+                if (Main.npc[i].active && !Main.npc[i].dontTakeDamage)
+                {
+                    Vector2 newMove = Main.npc[i].Center - projectile.Center;
+                    float DistanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+                    if (Distance > DistanceTo)
+                    {
+                        move = newMove;
+                        Distance = DistanceTo;
+                        LockOn = true;
+                    }
+
+                }
+            }
+            if (LockOn)
+            {
+                AdjustMagnitude(ref move);
+                projectile.velocity = (10 * projectile.velocity + move) / 40f;
+                AdjustMagnitude(ref projectile.velocity);
+
+            }
+        }
+        private void AdjustMagnitude(ref Vector2 vector)
+        {
+            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude < 6f)
+            {
+                vector *= 6f / magnitude;
+            }
+        }
+    }
+}
